d57bd8c87bafce297232d4ccd2444c832ccc4c46
SHuds1999 <52520173+SHuds1999@users.noreply.github.com>
2021-03-17 09:28:46 -0500

    Repo is made and ready to be used. Working on setting up methods and variables so i can code.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SuperHeroCreatorProj.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperHeroCreatorProj.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {            
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+
+    }
+}
---
+++ Models/SuperHero.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHeroCreatorProj.Models
+{
+    public class SuperHero
+    {
+        
+        public string Name { get; set; }
+        public string AlterEgo { get; set; }
+        
+        public string PrimarySuperHeroAbility { get; set; }
+
+        public string SecondarySuperHeoAbility { get; set; }
+
+        public string CatchPhrase { get; set; }
+
+    }
+}
